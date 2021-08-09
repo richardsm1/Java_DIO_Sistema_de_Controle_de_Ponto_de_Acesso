c886402a44ec3be5055b5d59f46429608fbc1347
Ricardo Murca <richard_sm1@outlook.com>
2021-08-09 02:57:55 -0300

    Criada a classe Calendario

---
+++ src/main/java/com/dio/live/model/Calendario.java
@@ -0,0 +1,19 @@
+package com.dio.live.model;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class Calendario {
+    private Long id;
+    private TipoData tipoData;
+    private String descricao;
+    private LocalDateTime dataEspecial;
+
+}
