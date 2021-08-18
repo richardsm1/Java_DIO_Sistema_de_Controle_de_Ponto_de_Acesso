d870e477e1fe106dcdcd3c7b83b19b3e914b3c3f
Ricardo Murca <richard_sm1@outlook.com>
2021-08-18 18:15:44 -0300

    Adicionadas annotations de auditoria

--- src/main/java/com/dio/live/controller/JornadaTrabalhoController.java
+++ src/main/java/com/dio/live/controller/JornadaTrabalhoController.java
@@ -3,6 +3,7 @@ package com.dio.live.controller;
 import com.dio.live.model.JornadaTrabalho;
 import com.dio.live.service.JornadaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,7 +13,6 @@ import java.util.NoSuchElementException;
 @RestController
 @RequestMapping("/jornada")
 public class JornadaTrabalhoController {
-
     @Autowired
     JornadaService jornadaService;
 
@@ -24,10 +24,31 @@ public class JornadaTrabalhoController {
     @GetMapping
     public List<JornadaTrabalho> getJornadaList(){
         return jornadaService.findAll();
+
     }
 
     @GetMapping("/{idJornada}")
-    public ResponseEntity<JornadaTrabalho> getJornadaById(@PathVariable("idJornada") Long idJornada) throws Exception {
-        return ResponseEntity.ok().body(jornadaService.getById(idJornada).orElseThrow(() -> new NoSuchElementException("Not found")));
+    public ResponseEntity<JornadaTrabalho>  getJornadaByID(@PathVariable("idJornada") Long idJornada) throws Exception {
+        return  ResponseEntity.ok(jornadaService.getById(idJornada).orElseThrow(() -> new NoSuchElementException("Not found!")));
+
     }
+
+    @PutMapping
+    public JornadaTrabalho updateJornada(@RequestBody JornadaTrabalho jornadaTrabalho){
+        return jornadaService.updateJornada(jornadaTrabalho);
+    }
+
+    @DeleteMapping("/{idJornada}")
+    public ResponseEntity deleteByID(@PathVariable("idJornada") Long idJornada) throws Exception {
+        try {
+            jornadaService.deleteJornada(idJornada);
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        return (ResponseEntity<JornadaTrabalho>) ResponseEntity.ok();
+
+    }
+
+
+
 }
